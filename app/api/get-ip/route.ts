9e4eef53bbdaa2ff85760199516c1541645e4de3
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2025-01-22 17:07:49 -0500

    Projectt

--- app/api/get-ip/route.ts
+++ app/api/get-ip/route.ts
@@ -2,5 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
+
+
+  console.log(ip);
+
   return NextResponse.json({ ip });
 }
