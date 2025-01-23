85b5ea7ed8d9a2cb506ff0044f5c652ef7e92943
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2025-01-23 08:58:49 -0500

    Project

--- app/api/get-ip/route.ts
+++ app/api/get-ip/route.ts
@@ -3,8 +3,37 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
 
-
   console.log(ip);
 
-  return NextResponse.json({ ip });
+  try {
+    const response = await fetch(`https://ip.wld.lol/${ip}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Error de la API: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    return NextResponse.json({ ip, data });
+  } catch (error) {
+    console.error("Error al obtener la IP:", error);
+
+    return new NextResponse(
+      JSON.stringify({ error: "Error al procesar la petición" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
